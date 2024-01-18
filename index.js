d578d0f223c04683bc1fa36475cb62c5abc04cda
Yusuf Abdullatif <107510170+Igneyous@users.noreply.github.com>
2024-01-18 15:13:35 +0100

    learning REST

--- index.js
+++ index.js
@@ -2,11 +2,40 @@ const express = require('express')
 const app = express()
 
 const stores = require('./stores.json')
-const port = 3000;
 
-app.get('/', (req, res) => {
-    res.json(stores);
+app.get('/stores/:storename', function (req, res) {
+  const { storename } = req.params;
+
+  const index = stores.findIndex(store => store.name === storename)
+  console.log('Store index is ${storename}');
+  if (index > -1) {
+    res.json(stores[index])
+  } else {
+    res.send('Store not found!')
+  }
 })
+
+app.delete('/', function (req, res) {
+  const { storename } = req.query
+  console.log(storename)
+  const index = stores.findIndex(store => store.name === storename)
+  if (index > -1) {
+    stores.splice(index, 1)
+    res.send(`Store found! Deleting store with index: ${index}`)
+  } else {
+    res.send('Store not found!')
+  }
+})
+
+app.post('/',
+  express.json(), // for parsing application/json body in POST
+  (req, res) => {
+    const { body } = req
+    console.log(body)
+    stores.push(body)
+    res.send('Store added!')
+})
+
 app.listen(3000, () => {
   console.log('Server is running at port 3000')
 })
