bc59a68b1040a5a6f31a4562e2bcc4f17d1bc8f6
Yusuf Abdullatif <107510170+Igneyous@users.noreply.github.com>
2024-01-25 16:22:56 +0100

    Linked database

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
+
 const express = require('express')
+require('dotenv').config();
 const app = express()
 
 const {Pool} = require('pg');
+const pool = new Pool({
+  user: 'postgres',
+  host: 'localhost',
+  database: 'postgres',
+  password: process.env.PGPASSWORD,
+  port: 5432,
+});
 
 const store = require('./routes/store/index.js')
 app.use('/', store)
@@ -9,7 +18,11 @@ app.use('/', store)
 
 const server = async() => 
 {
-  
+  await pool.connect();
+
+  const{rows} = await pool.query('SELECT * FROM public.stores');
+  console.log(rows);
+
   app.listen(3000, () => {
     console.log('Server is running at port 3000')
   });
