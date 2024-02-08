ebce89573145fbde8c07cf0a679e5bb2bdb885b6
Yusuf Abdullatif <107510170+Igneyous@users.noreply.github.com>
2024-02-08 15:05:33 +0100

    Added user authorization

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 
 const express = require('express')
+const cookieParser=require('cookie-parser');
 const app = express()
 const ModelClass = require('./model.js');
 const Model = new ModelClass();
@@ -9,6 +10,28 @@ const port =  process.env.PORT || 3000;
 
 const store = require('./routes/store/index.js')
 app.use('/', store)
+app.use(cookieParser());
+
+app.get('/login', async (req,res) =>{
+  const {username, password} = req.query;
+  if(username === 'yusuf' && password === '12345'){
+    res.cookie('token', 'super-secret-cookie',{httpOnly: true})
+    res.send('Login successful');
+  } else{
+    res.status(401).send("Invalid username or password");
+  }
+
+});
+
+app.get('/protected', async(req, res) => {
+  const{token} = req.cookies;
+  if(token === 'super-secret-cookie'){
+    res.send('User is logged in!');
+  }else{
+    res.status(401).send('Unauthorized');
+  }
+  res.send('Protected route');
+})
 
 
 const server = async() => 
