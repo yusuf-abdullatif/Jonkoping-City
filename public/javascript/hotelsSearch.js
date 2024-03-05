b61302be3a8d8140c041348404782f0780fa92df
VENGADESSANE <subburaman04@gmail.com>
2024-03-05 16:57:56 +0100

    store/hotel/restaurant search OK frontEnd

---
+++ public/javascript/hotelsSearch.js
@@ -0,0 +1,35 @@
+// Assuming hotels is an array of objects with a 'name' property
+fetch('http://localhost:3000/hotels/hotelsByNameStartsWith?hotelname='+window.searchText)
+    .then(response => response.json())
+    .then(hotels => {
+        displayHotels(hotels, 'storesContainer');
+    });
+
+function displayHotels(hotels, containerId) {
+    const oldContainer = document.getElementById(containerId);
+    oldContainer.innerHTML = '';
+    const container = document.getElementById(containerId);
+
+    hotels.forEach(hotel => {
+        const hotelBox = document.createElement('div');
+        hotelBox.classList.add('store-box');
+        hotelBox.style.maxHeight = '200px';
+        hotelBox.id = `letter-${hotel.name[0].toLowerCase()}`; // Set an ID based on the first letter
+
+        const nameElement = document.createElement('h2');
+        nameElement.textContent = hotel.name;
+        const infoElement = document.createElement('p');
+        infoElement.textContent = `Rating: ${hotel.rating || 'N/A'}`;
+        infoElement.innerHTML += `<br><br>${hotel.address || 'N/A'}`;
+
+        const urlElement = document.createElement('a');
+        urlElement.href = hotel.website;
+        urlElement.textContent = 'Visit Website';
+
+        hotelBox.appendChild(nameElement);
+        hotelBox.appendChild(infoElement);
+        hotelBox.appendChild(urlElement);
+
+        container.appendChild(hotelBox);
+    });
+}
---
+++ public/javascript/restaurantsSearch.js
@@ -0,0 +1,38 @@
+// Assuming restaurants is an array of objects with a 'name' property
+console.log("ss loaead")
+
+fetch('http://localhost:3000/restaurants/restaurantsByNameStartsWith?restaurantname='+window.searchText)
+    .then(response => response.json())
+    .then(restaurants => {
+        displayRestaurants(restaurants, 'storesContainer');
+    });
+
+function displayRestaurants(restaurants, containerId) {
+    const oldContainer = document.getElementById(containerId);
+    oldContainer.innerHTML = '';
+    const container = document.getElementById(containerId);
+
+    restaurants.forEach(restaurant => {
+        const restaurantBox = document.createElement('div');
+        restaurantBox.classList.add('store-box');
+        restaurantBox.style.maxHeight = '200px';
+        restaurantBox.id = `letter-${restaurant.name[0].toLowerCase()}`; // Set an ID based on the first letter
+
+        const nameElement = document.createElement('h2');
+        nameElement.textContent = restaurant.name;
+        const infoElement = document.createElement('p');
+        infoElement.textContent = `Rating: ${restaurant.rating || 'N/A'}`;
+        infoElement.innerHTML += `<br>Type: ${restaurant.restaurant_type || 'N/A'}`;
+        infoElement.innerHTML += `<br><br>${restaurant.address || 'N/A'}`;
+
+        const urlElement = document.createElement('a');
+        urlElement.href = restaurant.website;
+        urlElement.textContent = 'Visit Website';
+
+        restaurantBox.appendChild(nameElement);
+        restaurantBox.appendChild(infoElement);
+        restaurantBox.appendChild(urlElement);
+
+        container.appendChild(restaurantBox);
+    });
+}
