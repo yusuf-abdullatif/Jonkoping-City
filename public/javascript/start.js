388be57691c238db3da7288057293dfcfe77900d
VENGADESSANE <subburaman04@gmail.com>
2024-03-11 17:51:04 +0100

    add chart

--- public/javascript/start.js
+++ public/javascript/start.js
@@ -2,6 +2,45 @@ fetch('http://localhost:3000/stores/all')
     .then(response => response.json())
     .then(stores => {
         console.log(stores)
+
+        var keysArray = Object.keys(stores);
+        var size = keysArray.length;
+        
+        var rectY = 350-size         
+        var rectElement = document.querySelector('.NumberOfStores');
+
+        rectElement.setAttribute('y', rectY);
+        rectElement.setAttribute('height', size);
+    });
+
+    fetch('http://localhost:3000/restaurants/all')
+    .then(response => response.json())
+    .then(stores => {
+        console.log(stores)
+
+        var keysArray = Object.keys(stores);
+        var size = keysArray.length;
+        
+        var rectY = 350-size         
+        var rectElement = document.querySelector('.NumberOfRestaurants');
+
+        rectElement.setAttribute('y', rectY);
+        rectElement.setAttribute('height', size);
+    });
+
+    fetch('http://localhost:3000/hotels/all')
+    .then(response => response.json())
+    .then(stores => {
+        console.log(stores)
+
+        var keysArray = Object.keys(stores);
+        var size = keysArray.length;
+        
+        var rectY = 350-size         
+        var rectElement = document.querySelector('.NumberOfHotels');
+
+        rectElement.setAttribute('y', rectY);
+        rectElement.setAttribute('height', size);
     });
 
 
